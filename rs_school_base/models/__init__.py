d2b71dbc924c1a247eec60ee89b6b3932499e407
Matias Josafat Vera Martinez <matiasjosafatv@gmail.com>
2024-11-08 12:01:24 -0600

    [IMP] School actions and views and student new field

--- rs_school_base/models/__init__.py
+++ rs_school_base/models/__init__.py
@@ -1,3 +1,3 @@
 from . import rs_subject
 from . import rs_student
-from . import rs_teacher
+#from . import rs_teacher
--- rs_school_base/models/rs_student.py
+++ rs_school_base/models/rs_student.py
@@ -12,8 +12,16 @@ from odoo.exceptions import UserError, ValidationError
 class RsStudent(models.Model):
     _inherit = 'res.partner'
     
-    rs_student = fields.Boolean('Student?')
-    
-
-  
-   
+    rs_student = fields.Boolean('Student?')        
+    rs_blood_type = fields.Selection(
+        string='Blood Type',
+        selection=[('a_positive', 'A+'),
+        ('a_negative', 'A-'),
+        ('b_positive', 'B+'),
+        ('b_negative', 'B-'),
+        ('o_positive', 'O+'),
+        ('o_negative', 'O-'),
+        ('ab_positive', 'AB+'),
+        ('ab_negative', 'AB-')]
+    )
+    rs_teacher = fields.Boolean('Teacher?')
