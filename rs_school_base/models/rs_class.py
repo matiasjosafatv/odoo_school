9c014d4fe1b15cd548de51d8b4dd457c615d8501
Matias Josafat Vera Martinez <matiasjosafatv@gmail.com>
2024-11-18 19:23:20 -0600

    [IMP] *serch view rs_class *Added re_class search view *Added SerchPanel of rs_class

--- rs_school_base/models/rs_class.py
+++ rs_school_base/models/rs_class.py
@@ -4,7 +4,7 @@ class RSClass(models.Model):
 	_name='rs.class'
 	_rec_name='course_name'
     
-	course_name = fields.Char(string="Title", required=True)
+	course_name = fields.Char(string="Course", required=True)
 	description = fields.Text()
 	responsible_id = fields.Many2one('res.users', ondelete='set null', string="Course Taker", index=True)
 	session_ids = fields.One2many('rs.registration', 'course_id', string='Sessions')
