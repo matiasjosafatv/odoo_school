3b2de35db7087f3420044729d1215af7686e58a4
Matias Josafat <matiasjosafatv@gmail.com>
2024-11-20 19:26:18 +0000

    [IMP] How to inherit write method

--- rs_school_base/models/rs_registration.py
+++ rs_school_base/models/rs_registration.py
@@ -6,30 +6,30 @@ from odoo.exceptions import UserError, ValidationError
 class RsRegistration(models.Model):
 	_name='rs.registration'
 	_rec_name='session_name'
- 
+
 	session_name= fields.Char(required=True)
 	start_date = fields.Date(default=fields.Date.context_today)
 	duration = fields.Float(digits=(6,2), help="Duration in days", default= 1.0)
 	seats = fields.Integer(string="Number of seats")
 	active = fields.Boolean(default=True)
 	color = fields.Integer()
-	instructor_id = fields.Many2one('res.partner', string='Course Instructor', 
+	instructor_id = fields.Many2one('res.partner', string='Course Instructor',
                                     domain=[('rs_teacher', '=', True)])
-    
+
 	course_id = fields.Many2one('rs.class', ondelete='cascade', string='Course', required=True)
-    
+
 	attendee_ids = fields.Many2many('res.partner', string="Attendees")
-    
+
 	state = fields.Selection([('draft', 'Draft'), ('confirmed', 'Confirmed'), ('done','Done')])
- 
+
 	attendees_count = fields.Integer(string='Attendees Count', compute='calc_attendees', store=True)
- 
+
 	hours = fields.Float(string="Duration in hours", compute='calc_hours', inverse='set_hours')
- 
+
 	end_date = fields.Date(string="End Date", store=True, compute='get_end_date',inverse='set_end_date')
- 
+
 	rs_test = fields.Boolean(string='')
-	
+
 	@api.onchange('start_date')
 	def _onchange_start_date(self):
 		if self.start_date:
@@ -41,12 +41,12 @@ class RsRegistration(models.Model):
 	def calc_attendees(self):
 		for rec in self:
 			rec.attendees_count = len(rec.attendee_ids)
-   
+
 	@api.onchange('seats','attendee_ids')
 	def verify_valid_seats(self):
 		if self.seats < 0:
 			return {
-				'warning': { 
+				'warning': {
 					'title': "Incorrect amount for seats",
 					'message': "No. of seats can't be negative"
 				},
@@ -58,7 +58,7 @@ class RsRegistration(models.Model):
 					'message': "More Attendees than seats"
 				}
 			}
-   
+
 	@api.constrains('instructor_id', 'attendee_ids')
 	def check_instructor_not_in_attendees(self):
 		for rec in self:
@@ -73,15 +73,15 @@ class RsRegistration(models.Model):
 	def set_hours(self):
 		for rec in self:
 			rec.duration = rec.hours / 24
-   
-	@api.depends('duration', 'start_date')
+
+	@api.depends('start_date')
 	def get_end_date(self):
 		for record in self:
 			if not(record.start_date and record.duration):
 				record.end_date = record.start_date
 			else:
 				record.end_date = fields.Date.add(record.start_date,days=record.duration)
-   
+
 	def set_end_date(self):
 		for rec in self:
 			if rec.end_date:
@@ -93,6 +93,22 @@ class RsRegistration(models.Model):
 						end_date = rec.end_date
 						rec.duration = (end_date - start_date).days + 1
 					else:
-						rec.duration = 1.0					
+						rec.duration = 1.0
 						rec.start_date = fields.Date.add(rec.end_date,days=-rec.duration)
-   
+
+	def write(self, values):
+		"""
+            Update all record(s) in recordset, with new value comes as {values}
+            return True on success, False otherwise
+
+            @param values: dict of new values to be set
+
+            @return: True on success, False otherwise
+		"""
+		#import wdb;wdb.set_trace()
+		if values.get('session_name',None):
+			if values['session_name'] == 'Matias':
+				values['session_name'] = f"{values['session_name']} Josafat"
+		result = super(RsRegistration, self).write(values)
+
+		return result
